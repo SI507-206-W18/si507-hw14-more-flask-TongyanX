792f23c37d6885b7d6ba8a4452ef836d717ae67e
Tongyan <tongyan@umich.edu>
2018-04-18 15:43:53 -0400

    Normal & Extra Credit

--- app.py
+++ app.py
@@ -3,15 +3,18 @@ import model
 
 app = Flask(__name__)
 
+
 @app.route("/")
 def index():
     ## print the guestbook
     return render_template("index.html", entries=model.get_entries())
 
-@app.route("/add")
+
+@app.route("/add/")
 def addentry():
     ## add a guestbook entry
     return render_template("addentry.html")
+
 
 @app.route("/postentry", methods=["POST"])
 def postentry():
@@ -20,6 +23,31 @@ def postentry():
     model.add_entry(name, message)
     return redirect("/")
 
-if __name__=="__main__":
+
+@app.route("/admin/")
+def admin():
+    """?"""
+    # print the guestbook
+    return render_template("admin.html", entries=model.get_entries())
+
+
+@app.route("/delete", methods=["POST"])
+def deleteentry():
+    """?"""
+    post_id = request.form["id"]
+    model.delete_entry(post_id)
+    return redirect("/admin/")
+
+
+@app.route("/edit", methods=["POST"])
+def editentry():
+    """?"""
+    post_id = request.form["id"]
+    message = request.form["message"]
+    model.edit_entry(post_id, message)
+    return redirect("/admin/")
+
+
+if __name__ == "__main__":
     model.init()
     app.run(debug=True)
--- model.py
+++ model.py
@@ -4,32 +4,74 @@ from datetime import datetime
 
 GUESTBOOK_ENTRIES_FILE = "entries.json"
 entries = []
+next_id = 0
 
-def init(app):
-    global entries
+
+def init():
+    global entries, next_id
     try:
-
         f = open(GUESTBOOK_ENTRIES_FILE)
         entries = json.loads(f.read())
         f.close()
+        for entry in entries:
+            if entry["id"] > next_id:
+                next_id = entry["id"] + 1
     except:
         print('Couldn\'t open', GUESTBOOK_ENTRIES_FILE)
         entries = []
+
 
 def get_entries():
     global entries
     return entries
 
+
 def add_entry(name, text):
-    global entries, GUESTBOOK_ENTRIES_FILE
+    global entries, GUESTBOOK_ENTRIES_FILE, next_id
     now = datetime.now()
     time_string = now.strftime("%b %d, %Y %-I:%M %p")
-    entry = {"author": name, "text": text, "timestamp": time_string}
+    entry = {"author": name, "text": text, "timestamp": time_string, "id": next_id}
     entries.insert(0, entry) ## add to front of list
     try:
         f = open(GUESTBOOK_ENTRIES_FILE, "w")
         dump_string = json.dumps(entries)
         f.write(dump_string)
         f.close()
+        next_id += 1
     except:
+        print("ERROR! Could not write entries to file.")
+
+
+def delete_entry(post_id):
+    """?"""
+    global entries, GUESTBOOK_ENTRIES_FILE
+    for entry in entries:
+        if entry["id"] == int(post_id):
+            entries.remove(entry)
+            break
+    try:
+        f = open(GUESTBOOK_ENTRIES_FILE, "w")
+        dump_string = json.dumps(entries)
+        f.write(dump_string)
+        f.close()
+    except ValueError:
+        print("ERROR! Could not write entries to file.")
+
+
+def edit_entry(post_id, message):
+    """?"""
+    global entries, GUESTBOOK_ENTRIES_FILE
+    for entry in entries:
+        if entry["id"] == int(post_id):
+            entry["text"] = message
+            now = datetime.now()
+            time_string = now.strftime("%b %d, %Y %-I:%M %p")
+            entry["timestamp"] = time_string
+            break
+    try:
+        f = open(GUESTBOOK_ENTRIES_FILE, "w")
+        dump_string = json.dumps(entries)
+        f.write(dump_string)
+        f.close()
+    except ValueError:
         print("ERROR! Could not write entries to file.")
